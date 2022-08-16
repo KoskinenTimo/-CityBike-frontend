50adc8bb93ac25a0b33b6b021062db06dbc35a3c
Timo Koskinen <koskinentht@gmail.com>
2022-08-16 19:56:57 +0300

    changes to styles, half done

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-
 const queryClient = new QueryClient();
 
 function App() {
+
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <Router>
-          <HomePage />
+            <HomePage />
         </Router>
       </QueryClientProvider>
     </>
